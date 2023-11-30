b273c86c7282513e3ebaa01b8c369fae7b8a8311
Rea Shenjtolli <rei.shenjtolli@gmail.com>
2023-11-30 23:24:55 +0100

    Fix svg upload file

--- src/App.tsx
+++ src/App.tsx
@@ -1,20 +1,34 @@
-import React from 'react';
 import Grid from '@mui/material/Unstable_Grid2';
 import SvgLoadButton from './components/SvgLoadButton';
-import { useState } from 'react';
+import React, { useState } from 'react';
+
 
 function App() {
-  const [haveSVG, setHaveSVG] = useState(true)
+  const [fileContent, setFileContent] = useState<string>('')
+
+  function RenderSvg(props: { svg: string }) {
+    return React.createElement('svg', {
+      dangerouslySetInnerHTML: { __html: props.svg },
+      className: 'max-w-full max-h-full'
+    });
+  }
 
   return (
     <div className='base-position'>
       <Grid container spacing={2}>
         <Grid xs={12}>
-          <SvgLoadButton>{haveSVG ? 'Load SVG' : 'Add SVG'}</SvgLoadButton>
+          <SvgLoadButton
+            setFileContent={setFileContent}>
+            {fileContent ? 'Load SVG' : 'Add SVG'}
+          </SvgLoadButton>
         </Grid>
         <Grid xs={8}>
           <div className='border border-base-gray h-md-500px flex items-center justify-center'>
-            <img className='max-w-full max-h-full' src='/developer.svg' />
+            {!fileContent &&
+              <div>Click "Add SVG" to put your svg here!</div>}
+            {fileContent &&
+              <RenderSvg svg={fileContent} />
+            }
           </div>
         </Grid>
         <Grid xs={4}>
--- src/components/SvgLoadButton.tsx
+++ src/components/SvgLoadButton.tsx
@@ -1,18 +1,56 @@
 import { Button } from '@mui/material'
-import React from 'react'
+import { useRef, ChangeEvent } from 'react'
 
 type Child = {
     children: String,
+    setFileContent: any
 }
 
-const SvgLoadButton = ({ children }: Child) => {
+const SvgLoadButton = ({ children, setFileContent }: Child) => {
+
+    const fileInputRef = useRef<HTMLInputElement>(null);
+
+    const handleButtonClick = () => {
+        if (fileInputRef.current) {
+            fileInputRef.current.click();
+        }
+    };
+
+    const handleFileChange = (e: ChangeEvent<HTMLInputElement>) => {
+        const selectedFile = e.target.files && e.target.files[0];
+        // Do something with the selected file, for example, read its content
+        if (selectedFile) {
+          const reader = new FileReader();
+    
+          reader.onload = (event) => {
+            const content = event.target?.result as string;
+            setFileContent(content);
+          };
+    
+          // Read the file as text
+          reader.readAsText(selectedFile);
+        }
+    };    
+
     return (
-        <Button
-            variant="contained"
-            className="bg-base-gray rounded-md"
-        >
-            {children}
-        </Button>
+        <>
+            <input
+                type="file"
+                ref={fileInputRef}
+                style={{ display: 'none' }}
+                onChange={handleFileChange}
+                hidden={true}
+                accept="image/svg+xml"
+            />
+            <Button
+                variant="contained"
+                className="bg-base-gray rounded-md"
+                onClick={handleButtonClick}
+
+            >
+                {children}
+            </Button>
+        </>
     )
 }
 
